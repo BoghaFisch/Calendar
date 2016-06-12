bf9e51568c293775ee2b7ae0e7094795a63b80f1
David Lindström <davidlindstrom1989@gmail.com>
2016-06-12 23:43:11 +0200

    Update Calendar.cpp

--- Calendar.cpp
+++ Calendar.cpp
@@ -1,6 +1,6 @@
 #include "Calendar.hpp"
-//#include "gregorian.hpp"		// Inkluderat till main - behövs annars ej
-//#include "julian.hpp"			// ---------------------------------------
+//#include "gregorian.hpp"
+//#include "julian.hpp"
 
 namespace lab2 {
 
@@ -158,26 +158,26 @@ int main() {
 	using namespace std;
 	Calendar<Gregorian> cal;
 	cal.set_date(2000, 12, 2);
-	cal.add_event("Basketträning", 4, 12, 2000);
-	cal.add_event("Basketträning", 11, 12, 2000);
-	cal.add_event("Nyårsfrukost", 1, 1, 2001);
-	cal.add_event("Första advent", 1);          // år = 2000, månad = 12
-	cal.add_event("Vårdagjämning", 20, 3);      // år = 2000
+	cal.add_event("BaskettrÃ¤ning", 4, 12, 2000);
+	cal.add_event("BaskettrÃ¤ning", 11, 12, 2000);
+	cal.add_event("NyÃ¥rsfrukost", 1, 1, 2001);
+	cal.add_event("FÃ¶rsta advent", 1);          // Ã¥r = 2000, mÃ¥nad = 12
+	cal.add_event("VÃ¥rdagjÃ¤mning", 20, 3);      // Ã¥r = 2000
 	cal.add_event("Julafton", 24, 12);
-	cal.add_event("Kalle Anka hälsar god jul", 24); // också på julafton
-	cal.add_event("Julafton", 24); // En likadan händelse samma datum ska
-								   // ignoreras och inte sättas in i kalendern
-	cal.add_event("Min första cykel", 20, 12, 2000);
-	cal.remove_event("Basketträning", 4);
-	std::cout << cal; // OBS! Vårdagjämning och första advent är
-					  // före nuvarande datum och skrivs inte ut
+	cal.add_event("Kalle Anka hÃ¤lsar god jul", 24); // ocksÃ¥ pÃ¥ julafton
+	cal.add_event("Julafton", 24); // En likadan hÃ¤ndelse samma datum ska
+								   // ignoreras och inte sÃ¤ttas in i kalendern
+	cal.add_event("Min fÃ¶rsta cykel", 20, 12, 2000);
+	cal.remove_event("BaskettrÃ¤ning", 4);
+	std::cout << cal; // OBS! VÃ¥rdagjÃ¤mning och fÃ¶rsta advent Ã¤r
+					  // fÃ¶re nuvarande datum och skrivs inte ut
 	std::cout << "----------------------------------------" << std::endl;
-	cal.remove_event("Vårdagjämning", 20, 3, 2000);
-	cal.remove_event("Kalle Anka hälsar god jul", 24, 12, 2000);
+	cal.remove_event("VÃ¥rdagjÃ¤mning", 20, 3, 2000);
+	cal.remove_event("Kalle Anka hÃ¤lsar god jul", 24, 12, 2000);
 	cal.set_date(2000, 11, 2);
 	if (!cal.remove_event("Julafton", 24)) {
 		std::cout << " cal.remove_event(\"Julafton\", 24) tar inte" << std::endl
-			<< " bort något eftersom aktuell månad är november" << std::endl;
+			<< " bort nÃ¥got eftersom aktuell mÃ¥nad Ã¤r november" << std::endl;
 	}
 	std::cout << "----------------------------------------" << std::endl;
 	//std::cout << cal;
